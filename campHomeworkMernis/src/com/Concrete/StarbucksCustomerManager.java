58359aa8aac5799a853cb03ef74eb5d3c9d8144d
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 16:23:30 +0300

    Create StarbucksCustomerManager.java

---
+++ campHomeworkMernis/src/com/Concrete/StarbucksCustomerManager.java
@@ -0,0 +1,25 @@
+package com.Concrete;
+
+import com.Abstracts.BaseCustomerManager;
+import com.Abstracts.CustomerCheckService;
+import com.Entities.Customer;
+
+public class StarbucksCustomerManager extends BaseCustomerManager  {
+
+	
+	private CustomerCheckService customerCheckService;
+	
+	public StarbucksCustomerManager(CustomerCheckService customerCheckService) {
+		super();
+		this.customerCheckService = customerCheckService;
+	}
+	 
+	@Override
+	public void save (Customer customer) {
+		if(customerCheckService.CheckIfRealPerson(customer)) {
+			super.save(customer);
+		}else {
+			System.out.println("Geçerli Bir Kiþi Deðil");
+		}
+	}
+}
