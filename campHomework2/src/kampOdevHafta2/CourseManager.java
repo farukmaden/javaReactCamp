e8cfeb22952c089d21a4098431d5a430a3c3370f
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 16:06:56 +0300

    Create CourseManager.java

---
+++ campHomework2/src/kampOdevHafta2/CourseManager.java
@@ -0,0 +1,10 @@
+package kampOdevHafta2;
+
+public class CourseManager {
+	public void courseAdd(Courses courses) {
+		System.out.println(courses.courseName + " Kursu eklendi");
+	}
+	public void courseDelete(Courses courses) {
+		System.out.println(courses.courseName + " Kursu silnidi");
+	}
+}
