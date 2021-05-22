33f3d58b88a59dd72ce34661aaae672dbad80c50
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 17:38:41 +0300

    Create JobTitlesService.java

---
+++ campHomeworkHrms/src/main/java/javakamp/hrms/business/abstracts/JobTitlesService.java
@@ -0,0 +1,11 @@
+package javakamp.hrms.business.abstracts;
+
+import java.util.List;
+
+import javakamp.hrms.entities.concretes.JobTitles;
+
+public interface JobTitlesService {
+	List<JobTitles> getAll();
+	
+
+}
