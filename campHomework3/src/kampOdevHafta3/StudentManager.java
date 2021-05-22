e03bdf2fca9b379932ad2b526b58051e852eab62
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 16:09:13 +0300

    Create StudentManager.java

---
+++ campHomework3/src/kampOdevHafta3/StudentManager.java
@@ -0,0 +1,11 @@
+package kampOdevHafta3;
+
+public class StudentManager {
+	public void addCourse(Student student) {
+		System.out.println(student.studentCourse + ":kurs alındı");
+	}
+	
+	public void deleteCourse (Student student) {
+		System.out.println(student.studentCourse + ":kurs silinidi");
+	}
+}
