20aa1e272ba9d6831e28295c2dbaf17b1b51b64b
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 16:11:42 +0300

    Create Player.java

---
+++ campHomework4/src/com/Entities/Player.java
@@ -0,0 +1,58 @@
+package com.Entities;
+
+public class Player {
+	public int id;
+	public String firstName;
+	public String lastName;
+	public String email;
+	public String password;
+	
+	public Player(int id, String firstName, String lastName, String email, String password) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
