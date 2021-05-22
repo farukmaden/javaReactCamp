5d1c23571927f51da3e6f21aef529d49cdc1c557
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 16:06:54 +0300

    Create Courses.java

---
+++ campHomework2/src/kampOdevHafta2/Courses.java
@@ -0,0 +1,12 @@
+package kampOdevHafta2;
+
+public class Courses {
+	public Courses(int id,String courseName,String courseTeacher) {
+		this.id=id;
+		this.courseName=courseName;
+		this.courseTeacher=courseTeacher;
+	}
+	int id;
+	String courseName;
+	String courseTeacher;
+}
