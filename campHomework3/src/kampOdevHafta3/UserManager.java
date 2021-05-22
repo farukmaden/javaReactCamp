c104c266606f3200d3e96f25a5299794d4bef310
farukmaden <77931457+farukmaden@users.noreply.github.com>
2021-05-22 16:09:09 +0300

    Create UserManager.java

---
+++ campHomework3/src/kampOdevHafta3/UserManager.java
@@ -0,0 +1,10 @@
+package kampOdevHafta3;
+
+public class UserManager {
+	public void add (User user) {
+		System.out.println(user.firstName + ":kullanýcý eklendi");
+	}
+	public void delete (User user) {
+		System.out.println(user.firstName + ":kullanýcý silindi");
+	}
+}
